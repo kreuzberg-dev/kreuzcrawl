b03ce8d55d9f6060279658f7620abffb4779dfe8
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2026-04-17 09:36:59 +0200

    fix: Elixir rustler 0.37 + rustler_crates, C# remove --no-build

--- packages/elixir/mix.exs
+++ packages/elixir/mix.exs
@@ -7,6 +7,7 @@ defmodule Kreuzcrawl.MixProject do
       version: "0.1.0-rc.2",
       elixir: "~> 1.14",
       compilers: [:rustler] ++ Mix.compilers(),
+      rustler_crates: [kreuzcrawl_nif: [mode: :release]],
       description: "High-performance web crawling engine",
       package: package(),
       deps: deps()
@@ -16,13 +17,15 @@ defmodule Kreuzcrawl.MixProject do
   defp package do
     [
       licenses: ["Elastic-2.0"],
-      links: %{"GitHub" => "https://github.com/kreuzberg-dev/kreuzcrawl"}
+      links: %{"GitHub" => "https://github.com/kreuzberg-dev/kreuzcrawl"},
+      files: ~w(lib native .formatter.exs mix.exs README* LICENSE*)
     ]
   end
 
   defp deps do
     [
-      {:rustler, "~> 0.34"},
+      {:rustler, "~> 0.37.0", optional: true, runtime: false},
+      {:rustler_precompiled, "~> 0.9"},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.40", only: :dev, runtime: false}
     ]
