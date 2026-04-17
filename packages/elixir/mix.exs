460fb2b4ea11ce124781fb4898f18699464afbf3
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2026-04-17 13:22:17 +0200

    fix: remove LICENSE* from Elixir package files (not present)

--- packages/elixir/mix.exs
+++ packages/elixir/mix.exs
@@ -17,7 +17,7 @@ defmodule Kreuzcrawl.MixProject do
     [
       licenses: ["Elastic-2.0"],
       links: %{"GitHub" => "https://github.com/kreuzberg-dev/kreuzcrawl"},
-      files: ~w(lib native .formatter.exs mix.exs README* LICENSE*)
+      files: ~w(lib native .formatter.exs mix.exs README*)
     ]
   end
 
