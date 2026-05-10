4a1bfd1d32d62ae4186836208db140e2ccb075a4
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2026-05-10 10:05:55 +0200

    chore: remove debug scratch files

--- crates/kreuzcrawl/examples/debug_validate.rs
+++
@@ -1,9 +0,0 @@
-fn main() {
-    let mut c = kreuzcrawl::CrawlConfig::default();
-    c.max_depth = Some(200);
-    println!("max_depth = {:?}", c.max_depth);
-    match c.validate() {
-        Ok(()) => println!("validate: OK"),
-        Err(e) => println!("validate: ERR: {e}"),
-    }
-}
