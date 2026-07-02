0e0a8fa5cea2fc2292eb75df52e2781d6a3eaa9b
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2026-07-02 13:30:18 +0200

    chore(release): 1.0.2

--- test_apps/swift_e2e/Package.swift
+++ test_apps/swift_e2e/Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     .iOS(.v16),
   ],
   dependencies: [
-    .package(url: "https://github.com/xberg-io/crawlberg", branch: "release/swift/1.0.1"),
+    .package(url: "https://github.com/xberg-io/crawlberg", branch: "release/swift/1.0.2"),
   ],
   targets: [
     .testTarget(
